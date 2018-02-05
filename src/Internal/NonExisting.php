79592df708df50ab8c9022c37e5d09cb50601689
Magnus Karlsson <magnus@fairtradingtech.com>
2018-02-05 13:31:57 +0100

    Creating Internal\NonExisting class.

---
+++ src/Internal/NonExisting.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Laborious\Internal;
+
+
+/**
+ * This class is used by Laborious\Model to keep track of a changed parameter,
+ * where the parameter was non-existing since before.
+ */
+class NonExisting {}
