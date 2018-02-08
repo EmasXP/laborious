79de908c2079a4fcceb60764a5742a5c04691865
Magnus Karlsson <magnus@fairtradingtech.com>
2018-02-08 09:22:08 +0100

    Adding more tests for insertions.

--- tests/InsertTest.php
+++ tests/InsertTest.php
@@ -50,19 +50,37 @@ final class InsertTest extends TestCase {
 		$this->assertTrue($user->id !== null);
 	}
 
+	public function testHavePrimaryIdAfterSavedEmptyModel()
+	{
+		$user = new User($this->db);
+		$user->save();
+
+		$this->assertTrue(isset($user->id));
+		$this->assertTrue($user->id !== null);
+	}
+
 	public function testHaveNotPrimaryIdBeforeSave()
 	{
 		$user = new User($this->db);
 		$this->assertFalse(isset($user->id));
+		$this->assertNull($user->id);
 	}
 
-	public function testHaveNotNonSetParamsAfterSave()
+	public function testNotNonSetParamsIsNullAfterSave()
 	{
 		$user = new User($this->db);
 		$user->email = "test@example.com";
 		$user->save();
-		$this->assertFalse(isset($user->username));
-		$this->assertFalse(isset($user->country_id));
+		$this->assertNull($user->country_id);
+	}
+
+	public function testSameValueAfterSave()
+	{
+		$user = new User($this->db);
+		$user->email = "foobar@example.com";
+		$user->save();
+
+		$this->assertEquals("foobar@example.com", $user->email);
 	}
 
 	public function testHaveSetParamsAfterSave()
@@ -72,6 +90,62 @@ final class InsertTest extends TestCase {
 		$user->save();
 		$this->assertTrue(isset($user->email));
 		$this->assertTrue($user->email !== null);
+	}
+
+	public function testDoesNotHaveUndefinedFieldBeforeSave()
+	{
+		$user = new User($this->db);
+		$this->assertFalse(isset($user->undefined_field));
+	}
+
+	public function testDoesNotHaveUndefinedFieldAfterSave()
+	{
+		$user = new User($this->db);
+		$user->save();
+		$this->assertFalse(isset($user->undefined_field));
+	}
+
+	public function testSetValues()
+	{
+		$user = new User($this->db);
+		$user->setValues(array(
+			"email" => "1234@example.com",
+			"country_id" => 12,
+		));
+
+		$this->assertEquals("1234@example.com", $user->email);
+		$this->assertEquals(12, $user->country_id);
+
+		$user->save();
+
+		$this->assertEquals("1234@example.com", $user->email);
+		$this->assertEquals(12, $user->country_id);
+	}
+
+	public function testSetValuesWithExpectedValues()
+	{
+		$user = new User($this->db);
+		$user->setValues(
+			array(
+				"email" => "98765@example.com",
+				"country_id" => 12,
+			),
+			array(
+				"email",
+			)
+		);
+
+		$this->assertEquals("98765@example.com", $user->email);
+		$this->assertTrue(isset($user->email));
+		$this->assertNull($user->country_id);
+		$this->assertFalse(isset($user->country_id));
+
+		$user->save();
+
+		$this->assertEquals("98765@example.com", $user->email);
+		$this->assertTrue(isset($user->email));
+		$this->assertNull($user->country_id);
+		$this->assertFalse(isset($user->country_id));
 	}
 
 }
